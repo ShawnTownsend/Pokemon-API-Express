42def82a161b69080ee1a1c0432c606b570b8e7b
Shawn Townsend <fr3sh9475@hotmail.com>
2022-06-29 18:40:14 -0400

    Fixing migration files

--- migrations/20220629193316_create_users_table.js
+++ migrations/20220629193316_create_users_table.js
@@ -3,21 +3,21 @@
  * @returns { Promise<void> }
  */
  exports.up = function (knex) {
-    return knex.schema.createTable('user', (table) => {
-      table.increments('id').primary();
-      table.text('name').notNullable();
-      table.text('bio').notNullable();
-      table.integer('age').notNullable();
-    })
+    // return knex.schema.createTable('user', (table) => {
+    //   table.increments('id').primary();
+    //   table.text('name').notNullable();
+    //   table.text('bio').notNullable();
+    //   table.integer('age').notNullable();
+    // })
   
-      .createTable('pokemon', (table) => {
-        table.increments('id').primary();
-        table.integer('user_id').references('id').inTable('user');
-        table.text('name').notNullable();
-        table.integer('hp').notNullable();
-        table.text('front');
-        table.text('back');
-      });
+    //   .createTable('pokemon', (table) => {
+    //     table.increments('id').primary();
+    //     table.integer('user_id').references('id').inTable('user');
+    //     table.text('name').notNullable();
+    //     table.integer('hp').notNullable();
+    //     table.text('front');
+    //     table.text('back');
+    //   });
   };
   
   /**
@@ -25,6 +25,6 @@
    * @returns { Promise<void> }
    */
   exports.down = function (knex) {
-    return knex.schema.dropTable('user').dropTable('pokemon');
+    // return knex.schema.dropTable('user').dropTable('pokemon');
   };
   
