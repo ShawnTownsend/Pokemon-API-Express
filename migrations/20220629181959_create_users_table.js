4473d5b7ec919d65496b557e9b5fda617d0a4ba4
Shawn Townsend <fr3sh9475@hotmail.com>
2022-06-29 18:29:14 -0400

    Comment out migration file

--- migrations/20220629181959_create_users_table.js
+++ migrations/20220629181959_create_users_table.js
@@ -1,30 +1,30 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
- exports.up = function (knex) {
-    return knex.schema.createTable('user', (table) => {
-      table.increments('id').primary();
-      table.text('name').notNullable();
-      table.text('bio').notNullable();
-      table.integer('age').notNullable();
-    })
+// /**
+//  * @param { import("knex").Knex } knex
+//  * @returns { Promise<void> }
+//  */
+//  exports.up = function (knex) {
+//     return knex.schema.createTable('user', (table) => {
+//       table.increments('id').primary();
+//       table.text('name').notNullable();
+//       table.text('bio').notNullable();
+//       table.integer('age').notNullable();
+//     })
   
-      .createTable('pokemon', (table) => {
-        table.increments('id').primary();
-        table.integer('user_id').references('id').inTable('user');
-        table.text('name').notNullable();
-        table.integer('hp').notNullable();
-        table.text('front');
-        table.text('back');
-      });
-  };
+//       .createTable('pokemon', (table) => {
+//         table.increments('id').primary();
+//         table.integer('user_id').references('id').inTable('user');
+//         table.text('name').notNullable();
+//         table.integer('hp').notNullable();
+//         table.text('front');
+//         table.text('back');
+//       });
+//   };
   
-  /**
-   * @param { import("knex").Knex } knex
-   * @returns { Promise<void> }
-   */
-  exports.down = function (knex) {
-    return knex.schema.dropTable('user').dropTable('pokemon');
-  };
+//   /**
+//    * @param { import("knex").Knex } knex
+//    * @returns { Promise<void> }
+//    */
+//   exports.down = function (knex) {
+//     return knex.schema.dropTable('user').dropTable('pokemon');
+//   };
   
